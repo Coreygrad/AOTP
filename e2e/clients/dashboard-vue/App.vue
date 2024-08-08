0dfe92e270d632eac42a55a9fd2d5965d73ad73b
Coreygrad <Coreygrad@yahoo.com>
2024-08-08 17:25:02 +0300

    Refactoring e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :AOTP11="AOTP11" />
+</template>
+
+<script>
+import Vue from 'vue'
+import AOTP11 from '@AOTP11/core'
+import { Dashboard } from '@AOTP11/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    AOTP11: () => new AOTP11(),
+  },
+}
+</script>
+
+<style src="@AOTP11/core/dist/style.css"></style>
+<style src="@AOTP11/dashboard/dist/style.css"></style>
